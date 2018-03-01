5bd3b43b1f61c67eb32b6e467412a2ebbdc6ed5c
Sabine <matisuo@gmail.com>
2018-03-01 21:55:25 +0100

    commenting ... a bit

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,23 +1,24 @@
 class EmailsController < ApplicationController
-	before_action :all_emails, only: [:index, :destroy]
-	before_action :set_emails, only: [:update, :destroy, :show]
-	respond_to :html, :js
+	before_action :all_emails, only: [:index, :destroy] 
+	before_action :set_emails, only: [:destroy, :show]
+	respond_to :html, :js #will call the show.js the same way it calls show.html
 
+#no index because the before action takes care of it
+
+#show set the "read:true" status in the database
   def show
     @email.read = true
     @email.save
 	end
 
-
-  def update
-    @email.update_attributes!(read: true)
-  end
-
+#destroy the params given email
   def destroy
     @email = Email.destroy(params[:id])
     redirect_to root_path   
   end
 
+
+#declare var that will be used in 4 functions
   private 
   def all_emails
       @emails = Email.all
